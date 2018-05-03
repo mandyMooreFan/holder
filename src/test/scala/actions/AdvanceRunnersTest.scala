0576cb6ac6df0ee235f98311ad7a9909c1404555
MandyMooreFan <markbeeb@gmail.com>
2018-05-03 16:22:20 -0400

    tests working.

--- src/test/scala/actions/AdvanceRunnersTest.scala
+++ src/test/scala/actions/AdvanceRunnersTest.scala
@@ -26,71 +26,77 @@ class AdvanceRunnersTest extends WordSpec with Matchers with TypeCheckedTripleEq
 
     "handle" should {
       "Bat->first" in {
-        val field = Field(defense, Bases(None, None, None, List(), Option(player3)))
+        val field = Field(defense, Bases(None, None, None, List(None), Option(player3)))
         val result = AdvanceRunners(field).handle(Result.SINGLE)
-        result should ===(Field(defense, Bases(Option(player3), None, None, List(), None)))
+        result should ===(Field(defense, Bases(Option(player3), None, None, List(None), None)))
       }
 
       "Bat->second (double)" in {
-        val field = Field(defense, Bases(None, None, None, List(), Option(player3)))
+        val field = Field(defense, Bases(None, None, None, List(None), Option(player3)))
         val result = AdvanceRunners(field).handle(Result.DOUBLE)
-        result should ===(Field(defense, Bases(None, Option(player3), None, List(), None)))
+        result should ===(Field(defense, Bases(None, Option(player3), None, List(None, None), None)))
       }
 
       "Bat->third (triple)" in {
-        val field = Field(defense, Bases(None, None, None, List(), Option(player3)))
+        val field = Field(defense, Bases(None, None, None, List(None), Option(player3)))
         val result = AdvanceRunners(field).handle(Result.TRIPLE)
-        result should ===(Field(defense, Bases(None, None, Option(player3), List(), None)))
+        result should ===(Field(defense, Bases(None, None, Option(player3), List(None, None, None), None)))
       }
 
       "Bat->home run" in {
-        val field = Field(defense, Bases(None, None, None, List(), Option(player3)))
+        val field = Field(defense, Bases(None, None, None, List(None), Option(player3)))
         val result = AdvanceRunners(field).handle(Result.HOMER)
-        result should ===(Field(defense, Bases(None, None, None, List(), None)))
+        result should ===(Field(defense, Bases(None, None, None, List(None, None, None, Option(player3)), None)))
       }
 
       "Bases loaded single" in {
-        val field = Field(defense, Bases(Option(player4), Option(player5), Option(player6), List(), Option(player3)))
+        val field = Field(defense, Bases(Option(player4), Option(player5), Option(player6), List(None), Option(player3)))
         val result = AdvanceRunners(field).handle(Result.SINGLE)
-        result should ===(Field(defense, Bases(Option(player3), Option(player4), Option(player5), List(), None)))
+        result should ===(Field(defense, Bases(Option(player3), Option(player4), Option(player5), List(Option(player6)), None)))
       }
 
       "Bases loaded double" in {
-        val field = Field(defense, Bases(Option(player4), Option(player5), Option(player6), List(), Option(player3)))
+        val field = Field(defense, Bases(Option(player4), Option(player5), Option(player6), List(None), Option(player3)))
         val result = AdvanceRunners(field).handle(Result.DOUBLE)
-        result should ===(Field(defense, Bases(None, Option(player3), Option(player4), List(), None)))
+        result should ===(Field(defense, Bases(None, Option(player3), Option(player4), List(Option(player6), Option(player5)), None)))
       }
 
       "Bases loaded triple" in {
-        val field = Field(defense, Bases(Option(player4), Option(player5), Option(player6), List(), Option(player3)))
+        val field = Field(defense, Bases(Option(player4), Option(player5), Option(player6), List(None), Option(player3)))
         val result = AdvanceRunners(field).handle(Result.TRIPLE)
-        result should ===(Field(defense, Bases(None, None, Option(player3), List(), None)))
+        result should ===(Field(defense, Bases(None, None, Option(player3), List(Option(player6), Option(player5), Option(player4)), None)))
       }
 
       "Bases loaded home run" in {
-        val field = Field(defense, Bases(Option(player4), Option(player5), Option(player6), List(), Option(player3)))
+        val field = Field(defense, Bases(Option(player4), Option(player5), Option(player6), List(None), Option(player3)))
         val result = AdvanceRunners(field).handle(Result.HOMER)
-        result should ===(Field(defense, Bases(None, None, None, List(), None)))
+        result should ===(Field(defense, Bases(None, None, None, List(Option(player6), Option(player5), Option(player4), Option(player3)), None)))
+      }
 
-        "Continuous singles" in {
-          val field1 = Field(defense, Bases(None, None, None, List(), Option(player3)))
-          val result1 = AdvanceRunners(field1).handle(Result.SINGLE)
-          result1 should ===(Field(defense, Bases(Option(player3), None, None, List(), None)))
+      "Continuous singles" when {
+        val field1 = Field(defense, Bases(None, None, None, List(None), Option(player3)))
+        val result1 = AdvanceRunners(field1).handle(Result.SINGLE)
+        val field2 = Field(defense, Bases(result1.bases.firstBase, result1.bases.secondBase,
+          result1.bases.thirdBase, List(None), Option(player4)))
+        val result2 = AdvanceRunners(field2).handle(Result.SINGLE)
+        val field3 = Field(defense, Bases(result2.bases.firstBase, result2.bases.secondBase,
+          result2.bases.thirdBase, List(None), Option(player5)))
+        val result3 = AdvanceRunners(field3).handle(Result.SINGLE)
+        val field4 = Field(defense, Bases(result3.bases.firstBase, result3.bases.secondBase,
+          result3.bases.thirdBase, List(None), Option(player6)))
+        val result4 = AdvanceRunners(field4).handle(Result.SINGLE)
 
-          val field2 = Field(defense, Bases(result1.bases.firstBase, result1.bases.secondBase,
-            result1.bases.thirdBase, List(), None))
-          val result2 = AdvanceRunners(field2).handle(Result.SINGLE)
-          result2 should ===(Field(defense, Bases(Option(player4), Option(player3), None, List(), None)))
-
-          val field3 = Field(defense, Bases(result2.bases.firstBase, result2.bases.secondBase,
-            result2.bases.thirdBase, List(), Option(player5)))
-          val result3 = AdvanceRunners(field3).handle(Result.SINGLE)
-          result3 should ===(Field(defense, Bases(Option(player5), Option(player4), Option(player3), List(), None)))
-
-          val field4 = Field(defense, Bases(result3.bases.firstBase, result3.bases.secondBase,
-            result3.bases.thirdBase, List(), Option(player6)))
-          val result4 = AdvanceRunners(field4).handle(Result.SINGLE)
-          result4 should ===(Field(defense, Bases(Option(player6), Option(player5), Option(player4), List(), None)))
+        "result 1" in {
+          result1 should ===(Field(defense, Bases(Option(player3), None, None, List(None), None)))
+        }
+        "result 2" in {
+          result2 should ===(Field(defense, Bases(Option(player4), Option(player3), None, List(None), None)))
+        }
+        "result 3" in {
+          result3 should ===(Field(defense, Bases(Option(player5), Option(player4), Option(player3), List(None), None)))
+        }
+        "result 4" in {
+          result4 should ===(Field(defense, Bases(Option(player6), Option(player5), Option(player4), List(Option(player3)), None)))
         }
       }
     }
