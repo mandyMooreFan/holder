275ea3a9afeff11de61d57a911c2a303c80a213b
mandyMooreFan <markbeeb@gmail.com>
2018-05-03 16:30:06 -0400

    Add position (#25)
    * Adjusted position on the field.
    Added homePlate so that advanceRunners can be modified to add.
    
    * Tests adjusted to compile, but now failing.
    
    * tests working.

--- src/main/scala/game/Field.scala
+++ src/main/scala/game/Field.scala
@@ -1,16 +1,22 @@
 package game
 
-import game.cards.PlayerCard
+import game.cards.{PlayerCard, Position}
 
 case class Field(defense: Defense, bases: Bases)
 
-case class Defense(firstBaseman: PlayerCard, secondBaseman: PlayerCard) {
-  require(firstBaseman != null, "A first base man is required")
-  require(secondBaseman != null, "A secondBaseman is required")
+case class Defense(firstBaseman: PlayerCard, secondBaseman: PlayerCard, thirdBaseman: PlayerCard, shortStop: PlayerCard,
+                   catcher: PlayerCard, leftField: PlayerCard, centerField: PlayerCard, rightField: PlayerCard) {
+  require(firstBaseman != null && firstBaseman.position.contains(Position.FIRST_BASE), "A first base man is required")
+  require(secondBaseman != null && secondBaseman.position.contains(Position.SECOND_BASE), "A secondBaseman is required")
+  require(thirdBaseman != null && thirdBaseman.position.contains(Position.THIRD_BASE), "A thirdBaseman is required")
+  require(shortStop != null && shortStop.position.contains(Position.SHORT_STOP), "A shortStop is required")
+  require(catcher != null && catcher.position.contains(Position.CATCHER), "A catcher is required")
+  require(leftField != null && leftField.position.contains(Position.LEFT_FIELD), "A leftField is required")
+  require(centerField != null && centerField.position.contains(Position.CENTER_FIELD), "A centerField is required")
+  require(rightField != null && rightField.position.contains(Position.RIGHT_FIELD), "A rightField is required")
 }
 
 case class Bases(firstBase: Option[PlayerCard], secondBase: Option[PlayerCard], thirdBase: Option[PlayerCard],
-                 atBat: Option[PlayerCard]) {
+                 homePlate: List[Option[PlayerCard]], atBat: Option[PlayerCard]) {
   require(atBat != null, "A batter is required")
 }
-
--- src/test/scala/game/FieldTest.scala
+++ src/test/scala/game/FieldTest.scala
@@ -6,11 +6,18 @@ import org.scalatest.{Matchers, WordSpec}
 
 class FieldTest extends WordSpec with Matchers with TypeCheckedTripleEquals {
   "A Field" when {
+    val allPostions = Array(Position.FIRST_BASE, Position.SECOND_BASE, Position.SHORT_STOP, Position.THIRD_BASE,
+      Position.CATCHER, Position.LEFT_FIELD, Position.CENTER_FIELD, Position.RIGHT_FIELD)
     val dumbRange = SafeRange(0 to 0)
     val playerCard1 = PlayerCard("123", "Xasz", "Reds", "2018", 8, PlayerType.PITCHER, Hand.LEFT, 1, None, None, None,
-      Array(Position.CATCHER, Position.PITCHER), PlayerChart(dumbRange, dumbRange, dumbRange, dumbRange, dumbRange, dumbRange, dumbRange, dumbRange))
-    val defense = Defense(playerCard1, playerCard1)
-    val field = Field(defense, Bases(None, None, None, Option(playerCard1)))
+      allPostions, PlayerChart(dumbRange, dumbRange, dumbRange, dumbRange, dumbRange, dumbRange, dumbRange, dumbRange))
+    val defense = Defense(playerCard1, playerCard1, playerCard1, playerCard1, playerCard1, playerCard1, playerCard1,
+      playerCard1)
+    val field = Field(defense, Bases(None, None, None, List(),Option(playerCard1)))
+
+    val pitchCard = PlayerCard("123", "Xasz", "Reds", "2018", 8, PlayerType.PITCHER, Hand.LEFT, 1, None, None, None,
+      Array(Position.PITCHER), PlayerChart(dumbRange, dumbRange, dumbRange, dumbRange, dumbRange, dumbRange, dumbRange,
+        dumbRange))
 
     "new" must {
 
@@ -19,11 +26,82 @@ class FieldTest extends WordSpec with Matchers with TypeCheckedTripleEquals {
           field.defense.copy(firstBaseman = null)
         }
       }
+      "have the firstBaseman position be first base" in {
+        an[IllegalArgumentException] shouldBe thrownBy {
+          field.defense.copy(firstBaseman = pitchCard)
+        }
+      }
       "have a SecondBaseman" in {
         an[IllegalArgumentException] shouldBe thrownBy {
           field.defense.copy(secondBaseman = null)
         }
       }
+      "have the SecondBaseman position be second base" in {
+        an[IllegalArgumentException] shouldBe thrownBy {
+          field.defense.copy(secondBaseman = pitchCard)
+        }
+      }
+      "have a ThirdBaseman" in {
+        an[IllegalArgumentException] shouldBe thrownBy {
+          field.defense.copy(thirdBaseman = null)
+        }
+      }
+      "have the thirdBaseman position be third base" in {
+        an[IllegalArgumentException] shouldBe thrownBy {
+          field.defense.copy(thirdBaseman = pitchCard)
+        }
+      }
+      "have a ShortStop" in {
+        an[IllegalArgumentException] shouldBe thrownBy {
+          field.defense.copy(shortStop = null)
+        }
+      }
+      "have the ShortStop position be shortstop" in {
+        an[IllegalArgumentException] shouldBe thrownBy {
+          field.defense.copy(shortStop = pitchCard)
+        }
+      }
+      "have a catcher" in {
+        an[IllegalArgumentException] shouldBe thrownBy {
+          field.defense.copy(catcher = null)
+        }
+      }
+      "have the catcher position be catcher" in {
+        an[IllegalArgumentException] shouldBe thrownBy {
+          field.defense.copy(catcher = pitchCard)
+        }
+      }
+      "have a LeftFielder" in {
+        an[IllegalArgumentException] shouldBe thrownBy {
+          field.defense.copy(leftField = null)
+        }
+      }
+      "have the LeftFielder position be left field" in {
+        an[IllegalArgumentException] shouldBe thrownBy {
+          field.defense.copy(leftField = pitchCard)
+        }
+      }
+      "have a RightFielder" in {
+        an[IllegalArgumentException] shouldBe thrownBy {
+          field.defense.copy(rightField = null)
+        }
+      }
+      "have the RightFielder position be right field" in {
+        an[IllegalArgumentException] shouldBe thrownBy {
+          field.defense.copy(rightField = pitchCard)
+        }
+      }
+      "have a CenterFielder" in {
+        an[IllegalArgumentException] shouldBe thrownBy {
+          field.defense.copy(centerField = null)
+        }
+      }
+      "have the CenterFielder position be center field" in {
+        an[IllegalArgumentException] shouldBe thrownBy {
+          field.defense.copy(centerField = pitchCard)
+        }
+      }
+
       "have a batter" in {
         an[IllegalArgumentException] shouldBe thrownBy {
           field.bases.copy(atBat = null)
